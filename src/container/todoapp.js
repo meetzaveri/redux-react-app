cfd13d95f342aa1ce2a3c37bd69f1349a7f0985d
Meet Zaveri <meethpatel96@gmail.com>
2018-01-10 16:37:52 +0530

    Update todoapp.js

--- src/container/todoapp.js
+++ src/container/todoapp.js
@@ -4,8 +4,10 @@ import store from '../store/store';
 import Todolist from './components/todolist';
 import AddTodo from './components/addtodo';
 import Footer from './components/footer';
+import { connect } from 'react-redux';
 
 var noteId = 0;
+
 const getVisibleTodos = (todos,filter) => {
     switch(filter){
         case 'SHOW_ALL' : 
@@ -23,22 +25,41 @@ class TodoApp extends React.Component{
 
     render() {
         const {todos,visibilityFilter} = this.props;
-        const visibileTodos = getVisibleTodos(todos,visibilityFilter);
+        
+        return(
             <div>
+                <AddTodo onAddClick={this.props.onAddTodoClick} />
                 <ul>
-                    <Todolist todos={visibileTodos} onTodoClick ={id => 
-                    store.dispatch({type:'TOGGLE_TODO',id})
-                    }/>
+                    <Todolist todos={this.props.todos} 
+                    onTodoClick ={this.props.onTodoListClick}/>
                 </ul>
-                
-              <Footer visibilityFilter={visibilityFilter} 
-                        onFilterClick={filter => {
-                store.dispatch({type :'SET_VISIBILITY_FILTER',filter});
-              }} />
-
+                <Footer visibilityFilter={this.props.visibilityFilter} 
+                        onFilterClick={this.props.onLinkClick} />
               <button onClick = {() => console.log(store.getState())}> Arbitrary Button </button>
             </div>
+        );
     }
 }
 
-export default TodoApp;
+const mapStateToProps = (state) => {
+    return {
+        visibilityFilter : state.visibilityFilter,
+        todos: getVisibleTodos(state.todos,state.visibilityFilter)
+    }
+}
+
+const mapDispatchToProps = (dispacth) => {
+    return {
+        onLinkClick : (filter) => {
+            store.dispatch({type :'SET_VISIBILITY_FILTER',filter});
+        },
+        onTodoListClick : (id) => {
+            store.dispatch({type:'TOGGLE_TODO',id});
+        },
+        onAddTodoClick : (text) => {
+            store.dispatch({type:'ADD_TODO',id : noteId++,text})
+        }
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(TodoApp);
