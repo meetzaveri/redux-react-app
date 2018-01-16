8aabeff6cd4bf2d98866d92dc328fad0b425cb11
Meet Zaveri <meethpatel96@gmail.com>
2018-01-16 14:49:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import store from './store/store';
 import TodoApp from './container/todoapp';
+import { Provider } from 'react-redux';
 
 const render = () => {
-    ReactDOM.render(<TodoApp todos={store.getState().todos} 
-    visibilityFilter ={store.getState().visibilityFilter}/>,document.getElementById('root'));
+    ReactDOM.render(
+    <Provider store={store}>
+        <TodoApp />
+    </Provider>
+    ,document.getElementById('root'));
 }
 
-store.subscribe(render);
 render();
 
 
